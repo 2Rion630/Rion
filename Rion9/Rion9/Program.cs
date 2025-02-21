cc138904f0523e956da3d2af924681814fa7ff2d
2Rion630 <hank92315@gmail.com>
2025-02-21 17:41:47 +0900

    text game

---
+++ Rion9/Rion9/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Rion9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //로딩바 시작화면
+            //게임 스토리 작성
+            Console.Write("시작하려면 엔터를 눌러주세요.");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("　　　　　　　　　  ");
+            Thread.Sleep(1000);                            
+            Console.Clear();                               
+            Console.WriteLine("≫　　　　　　　　≪");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("≫≫　　　　　　≪≪");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("≫≫≫　　　　≪≪≪");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("≫≫≫≫　　≪≪≪≪");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("≫≫≫≫≪≫≪≪≪≪");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("≫≫≫≪≪≫≫≪≪≪");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("≫≫≪≪≪≫≫≫≪≪");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("≫≪≪≪≪≫≫≫≫≪");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("≪≪≪≪≪≫≫≫≫≫");
+            Thread.Sleep(2000);
+            Console.Clear();
+            Console.WriteLine("≪≪≪로딩완료≫≫≫");
+            Console.Clear();
+            Console.WriteLine("리소스팩을확인중입니다");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("≫≫≫게임시작≪≪≪");
+            Console.Clear();
+
+            Console.WriteLine("●●●●●●●●●●●●●●●●●●●");
+            Console.WriteLine("●　　　　 　　　　●");
+            Console.WriteLine("●　Rion　 　RPG　 ●");
+            Console.WriteLine("●　　　 　 　　　 ●");
+            Console.WriteLine("●●●●●●●●●●●●●●●●●●●");
+            Thread.Sleep(4000);
+            Console.Clear();
+            Console.WriteLine("Rion RPG에 오신것을 환영합니다.");
+            Console.Write("캐릭터를 생성하려면 엔터를 눌러주세요.");
+            Console.ReadLine();
+            Console.WriteLine("캐릭터 이름을 입력하세요:");
+            string userName = Console.ReadLine();         
+            Console.WriteLine($"{userName}캐릭터가 생성되었습니다!");
+            Thread.Sleep(2000);
+            Console.Clear();
+            Console.WriteLine("낯선 마을에서 모험을 시작합니다.");
+            Thread.Sleep(3000);
+            Console.Clear();
+            Console.WriteLine("┌──────────┐");
+            Console.WriteLine("│리온  마을│");
+            Console.WriteLine("└──────────┘");
+            Thread.Sleep(4000);
+            Console.Clear();
+            Console.WriteLine($"{userName} : 처음보는 마을에 도착했다. 이름이 리온 마을?");
+            Thread.Sleep(4000);
+            Console.Clear();
+            Console.WriteLine($"{userName} : 일단 주변을 좀 둘러봐야겠어");
+            Thread.Sleep(4000);
+            Console.Clear();
+            Console.WriteLine("주변을 둘러보다 마을을 기습하는 멧돼지와 마주친다");
+            Thread.Sleep(4000);
+            Console.Clear();
+            Console.WriteLine($"무기가 없는 {userName}은(는) 도망치길 선택했다.");
+            Thread.Sleep(4000);
+            Console.Clear();
+            Console.WriteLine($"그렇게 마을은 쑥대밭이되고 {userName}은(는) 복수를 다짐한다.");
+            Thread.Sleep(4000);
+            Console.Clear();
+            Console.WriteLine($"복수를 위해 {userName}은(는) 다른 마을로 떠난다.");
+            Thread.Sleep(4000);
+            Console.Clear();
+            Console.WriteLine("★Rion RPG2 2050년 1월 1일 오픈 베타 출시 예정★");
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
