75cea5db0c2a0941ec6f843ceb9b559a54875db2
2Rion630 <hank92315@gmail.com>
2025-02-21 15:04:57 +0900

    study4

--- Rion7/Rion7/Program.cs
+++ Rion7/Rion7/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Rion7
@@ -10,6 +12,41 @@ namespace Rion7
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("□□□□□□□□□□");
+            Thread.Sleep(2000);                             //1000 = 1초
+            Console.Clear();                                //콘솔 화면 지우기
+            Console.WriteLine("■□□□□□□□□□");
+            Thread.Sleep(2000);                             
+            Console.Clear();
+            Console.WriteLine("■■□□□□□□□□");
+            Thread.Sleep(2000);                             
+            Console.Clear();                                
+            Console.WriteLine("■■■□□□□□□□");
+            Thread.Sleep(2000);
+            Console.Clear();
+            Console.WriteLine("■■■■□□□□□□");
+            Thread.Sleep(2000);
+            Console.Clear();
+            Console.WriteLine("■■■■■□□□□□");
+            Thread.Sleep(2000);
+            Console.Clear();
+            Console.WriteLine("■■■■■■□□□□");
+            Thread.Sleep(2000);
+            Console.Clear();
+            Console.WriteLine("■■■■■■■□□□");
+            Thread.Sleep(2000);
+            Console.Clear();
+            Console.WriteLine("■■■■■■■■□□");
+            Thread.Sleep(2000);
+            Console.Clear();
+            Console.WriteLine("■■■■■■■■■□");
+            Thread.Sleep(2000);
+            Console.Clear();
+            Console.WriteLine("■■■■■■■■■■");
+            Thread.Sleep(2000);
+            Console.Clear();
+            Console.WriteLine("■■■로딩완료■■■");
+
         }
     }
 }
