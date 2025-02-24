2638db9a7f5bca577495e9707145b8a38df7692a
2Rion630 <hank92315@gmail.com>
2025-02-24 10:33:08 +0900

    Day2_1

---
+++ Day2_1/Day2_1/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //이진수를 정수로 변환
+
+            //Console.Write("2진수를 입력하세요:");
+            //string binaryInput = Console.ReadLine();                 //입력받는다 문자영
+            //int decimalValue = Convert.ToInt32(binaryInput, 2);      //2진수 -> 10진수 변환
+
+            //정수를 이진수로 변환
+            //string binaryOutput = Convert.ToString(decimalValue, 2); //10진수 -> 2진수 변환
+
+            //Console.WriteLine($"입력한 이진수 : {binaryInput}");
+            //Console.WriteLine($"10진수로 변환 : {decimalValue}");
+            //Console.WriteLine($"다시 이진수로 변환 :{binaryOutput} ");
+
+
+            //var를 사용하여 변수 선언
+            //var name = "Alice";             //문자열로 추론
+            //var age = 25;                   //정수로 추론
+            //var isSudent = true;            //논리값으로 추론
+
+            //Console.WriteLine($"이름 : {name}, 나이 : {age}, 학생 여부 : {isSudent}");
+
+            //var는 읽기 쉽고 유연하지만, 남용하지 않도록 타입 추론을 명확히 이해
+
+
+            //default 키워드 사욘한 기본값 설정
+            //int defaultInt = default;           //기본값 : 0
+            //string defaultSteing = default;     //기본값 : null
+            //bool defaultBool = default;         //기본값 : false
+
+
+            //Console.WriteLine($"정수 기본값 : {defaultInt}");
+            //Console.WriteLine($"문자열 기본값 : {defaultSteing}");
+            //Console.WriteLine($"논리값 기본값 : {defaultBool}");
+
+            //default는 변수를 초기화하지 않을 경우 발생하는 오류를 방지하는 데 유용
+
+            //1.문자열 입력 메서드: 사용자로부터 값을 입력받고 처리
+            //2.형식 변환: 문자열을 정수 등으로 변환하여 숫자 연산 가능
+            //3.이진수 다루기: 이진수 변환으로 수학적 연산 확장
+            //4.var 키워드: 타입 추론을 통해 선언과 초기화를 간결하게
+            //5.default 키워드: 데이터 형식에 맞는 기본값을 설정
+
+            //연산자
+            //연산자는 값을 계산하거나 조작할 때 사용됩니다.
+            //단항, 산술, 관계형, 논리, 비트 연산자 등 다양한 종류가 있습니다.
+
+            //int a = 5, b = 3;
+            //int sum = 0;
+
+
+            //sum = a + b;                              //산술 연산자 사용
+            //Console.WriteLine($"합 : {sum}");         //출력 : 8
+            //sum = a - b;
+            //Console.WriteLine($"합 : {sum}");         //출력 : 2
+            //sum = a * b;
+            //Console.WriteLine($"합 : {sum}");         //출력 : 15
+            //sum = a / b;
+            //Console.WriteLine($"합 : {sum}");         //출력 : 1
+
+            //int c = 0, d = 0;
+            //c = 10;
+            //d = 3;
+            //sum = c % d;                              //나머지를 구하는 연산자
+            //Console.WriteLine($"합 : {sum}");         
+
+            //int number = 7;
+            //int sum = 0;
+            //sum = number % 2;                         //0이 나오면 짝수 1홀수
+
+            //Console.WriteLine("짝수 홀수 판별 : " + sum);
+
+            //bool isEqual = false;                       //거짓 0
+
+            //int a = 5;
+            //int b = 2;
+
+
+            //관계형 연산자 
+            //isEqual = (a == b);                         //a랑 b가 같은가?
+
+            //Console.WriteLine("같은가? :" + isEqual);
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
